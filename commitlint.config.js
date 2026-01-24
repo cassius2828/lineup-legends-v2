bfc8819a712c0efca48fb65469da89bbe0983f1b
Cassius <cassius.reynolds.dev@gmail.com>
2026-01-24 02:37:16 -0800

    chore: Disable subject-case, subject-full-stop, and header-max-length rules in commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,14 +19,10 @@ export default {
       ],
     ],
     "type-case": [2, "always", "lower-case"],
-    "subject-case": [
-      2,
-      "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"],
-    ],
+    "subject-case": [0], // Disabled
     "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 100],
+    "subject-full-stop": [0], // Disabled
+    "header-max-length": [0], // Disabled
   },
 };
 
