8f536d2da844b24fa82f6bb29357adf79c8c79b7
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2026-04-10 16:15:42 -0700

    fix: update grid layout for LineupCardGrid component
    - Changed the grid class from 'lg:grid-cols-2' to 'xl:grid-cols-2' to improve responsiveness and layout on larger screens.

--- src/app/_components/common/LineupCardGrid.tsx
+++ src/app/_components/common/LineupCardGrid.tsx
@@ -1,6 +1,6 @@
 const LineupCardGrid = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">{children}</div>
+    <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">{children}</div>
   );
 };
 export default LineupCardGrid;
